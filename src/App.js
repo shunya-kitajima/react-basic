95f8fd5180be5f6a07d5e4b2bf3266a8e1b0ed5f
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-03 21:54:15 +0900

    ファイルを追加,修正

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import Basic1 from "./components/Basic1";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+        <Basic1 name="Hooks" />
+        <Basic1 name="Today" />
       </header>
     </div>
   );
---
+++ src/components/Basic1.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Basic1(props) {
+  return (
+    <>
+      <h1>{props.name}</h1>
+      <h1>Hello React2</h1>
+    </>
+  );
+}
+
+export default Basic1;
