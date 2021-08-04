d2b348e47072bad2bb4136df6d5db40ad720a299
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-05 01:02:58 +0900

    ボタンクリック時の処理を追加

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 function Basic1(props) {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
   return (
     <>
+      <button onClick={clickHandler}>Click</button>
       <h1>{props.name}</h1>
       <h1>Hello React2</h1>
     </>
