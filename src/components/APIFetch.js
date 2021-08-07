cc771bc2308e0870c13987fd2c1c63affce5cea2
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-07 20:44:27 +0900

    urlにidを追加、postsのmapをコメントアウト

--- src/components/APIFetch.js
+++ src/components/APIFetch.js
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from "react";
 
 const APIFetch = () => {
   const [posts, setPosts] = useState([]);
+  const [id, setid] = useState(1);
+  const [clicked, setClicked] = useState(false);
 
   useEffect(() => {
     // axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
     //   setPosts(res.data);
     // });
-    fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" })
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { method: "GET" })
       .then((res) => res.json())
       .then((data) => {
         setPosts(data);
@@ -17,11 +19,11 @@ const APIFetch = () => {
 
   return (
     <div>
-      <ul>
+      {/* <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
